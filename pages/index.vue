4f410439581f4bed8e38201abe6bbb2c96de988b
Konstantin BIFERT <hello@kissu.io>
2022-11-01 11:09:55 +0100

    done

---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <div class="bg-primary-300">Welcome to the homepage</div>
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+const colors = require('tailwindcss/colors')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.green
+      }
+    },
+  },
+}
